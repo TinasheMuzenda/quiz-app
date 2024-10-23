a39f38ef6a1fafc5a786c9351ab25ab87adc9321
TinasheMuzenda <tmuzendat@gmail.com>
2024-10-23 11:00:25 +0200

    Tinashe Quiz

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Which car is Tinashe fond of?",
+        option1: "BMW M3",
+        option2: "Bugatti Veron",
+        option3: "Ford Shelby Mustang GT500",
+        option4: "Koenigsegg Jesko Attack",
+        ans: 3,
+    },
+    {
+        question: "What is his preferred destination after everything?",
+        option1: "Enjoy with family",
+        option2: "Run for President",
+        option3: "Sleep all the time",
+        option4: "Make more money",
+        ans: 1,
+    },
+    {
+        question: "What is his favorite color?",
+        option1: "Orange",
+        option2: "Yellow",
+        option3: "Charcoal",
+        option4: "Nude",
+        ans: 1,
+    },
+    {
+        question: "Preferred country to live?",
+        option1: "France",
+        option2: "Australia",
+        option3: "Qatar",
+        option4: "Zimbabwe",
+        ans: 4,
+    },
+    {
+        question: "Sports he played and enjoyed?",
+        option1: "Cricket,  Hockey, Athletics",
+        option2: "Baseball, Soccer, Cricket",
+        option3: "Soccer, Rugby, Tennis",
+        option4: "Basketball, Handball, Badminton",
+        ans: 2,
+    },
+]
